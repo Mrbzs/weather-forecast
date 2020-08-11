124d6b7aebfd490da90062435dda3372e517facd
Mrbzs <bzsababa@hotmail.com>
2020-08-11 19:12:47 +0300

    Updates

---
+++ precache-manifest.19e21181d92f238a385ecfda3032bc3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22138e90037213de624f8e9ce6b5127c",
+    "url": "/weather-forecast/index.html"
+  },
+  {
+    "revision": "c6b3ad111b12d6118f03",
+    "url": "/weather-forecast/static/css/2.ed7b5974.chunk.css"
+  },
+  {
+    "revision": "ff24f0ef6657d3780f97",
+    "url": "/weather-forecast/static/css/main.2f718051.chunk.css"
+  },
+  {
+    "revision": "c6b3ad111b12d6118f03",
+    "url": "/weather-forecast/static/js/2.5aa48a21.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/weather-forecast/static/js/2.5aa48a21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff24f0ef6657d3780f97",
+    "url": "/weather-forecast/static/js/main.368d2231.chunk.js"
+  },
+  {
+    "revision": "bd3109f8ee8d51476388",
+    "url": "/weather-forecast/static/js/runtime-main.63a2a2cf.js"
+  }
+]);
